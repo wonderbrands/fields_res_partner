49ebf2a3a724991e890065a4c873b6c657263838
SergioWB <sergio@wonderbrands.co>
2024-09-11 17:50:34 -0600

    [UPD] Cambio en nombre de campos

--- res_partner_extend/models/new_partner_fields.py
+++ res_partner_extend/models/new_partner_fields.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api, _
 class ResPartnerRES(models.Model):
     _inherit = "res.partner"
 
-    advance_payment_date = fields.Integer(string='Día de pago de anticipo', help='Cantidad de días de pago de anticipo')
+    advance_payment_date = fields.Integer(string='Días de financiamiento', help='Cantidad de días de financiamiento')
     percent_supplier_advance = fields.Float(string='% Anticipo proveedor', help='Muestra el porcentaje de anticipo al proveedor')
-    lt_ag_channel = fields.Integer(string='LT AGcanal', help='LT AGcanal')
+    lt_ag_channel = fields.Integer(string='LT Producción', help='LT Producción')
